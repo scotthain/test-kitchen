59943766a8604e94581904efde94eb2d9298808b
Scott Hain <shain@chef.io>
2015-03-10 14:57:04 -0700

    Fixed chef helper to work with sh as well as bash

--- support/chef_helpers.sh
+++ support/chef_helpers.sh
@@ -1,3 +1,5 @@
+set -x
+
 # @param $1 the omnibus root directory
 # @param $2 the requested version of omnibus package
 # @return 0 if omnibus needs to be installed, non-zero otherwise
@@ -7,7 +9,7 @@ should_update_chef() {
   elif test "$2" = "latest" ; then return 0
   fi
 
-  local version="`$1/bin/chef-solo -v | cut -d " " -f 2`"
+  version="`$1/bin/chef-solo -v | cut -d \" \" -f 2`"
   if echo "$version" | grep "^$2" 2>&1 >/dev/null; then
     return 1
   else
